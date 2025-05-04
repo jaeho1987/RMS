9e8c56cf9151aea20d6995fba3f8008a0e83b1e8
Jaeho Lee <jaeho_1987@naver.com>
2025-05-04 22:28:33 +0900

    수정

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,10 +1,72 @@
+import React, { Suspense, useEffect } from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { CSpinner, useColorModes } from '@coreui/react'
+import '../scss/style.scss'
+import '../scss/examples.scss'
+
+// Containers
+const DefaultLayout = React.lazy(() => import('../layout/DefaultLayout'))
+
+// Pages
+const Login = React.lazy(() => import('../views/pages/login/Login'))
+const Register = React.lazy(() => import('../views/pages/register/Register'))
+const Page404 = React.lazy(() => import('../views/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('../views/pages/page500/Page500'))
+
+// Components
+import routes from '../routes'
+import ProtectedRoute from '../components/ProtectedRoute'
+
+const App = () => {
+  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
+  const storedTheme = useSelector((state) => state.theme)
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
+    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
+    if (theme) {
+      setColorMode(theme)
+    }
+    if (!isColorModeSet()) {
+      setColorMode(storedTheme)
+    }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
+    <BrowserRouter>
+      <Suspense
+        fallback={
+          <div className="pt-3 text-center">
+            <CSpinner color="primary" variant="grow" />
+          </div>
+        }
+      >
         <Routes>
+          {/* 기본 공개 라우트 */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/404" element={<Page404 />} />
+          <Route path="/500" element={<Page500 />} />
+
+          {/* 인증 보호 라우트 */}
           {routes.map((route, idx) => {
+            if (!route.element) return null
+            const element = route.private ? (
+              <ProtectedRoute>{route.element}</ProtectedRoute>
+            ) : (
+              route.element
             )
+            return <Route key={idx} path={route.path} element={element} />
           })}
+
+          {/* 기본 리디렉트 */}
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </Suspense>
+    </BrowserRouter>
   )
 }
 
+export default App
