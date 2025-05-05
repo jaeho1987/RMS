6df04496ecdc1f332a570a5a0d9e6f35fa7493ea
Jaeho Lee <jaeho_1987@naver.com>
2025-05-06 02:15:05 +0900

    수정

---
+++ src/views/system/CodeManagement.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CompanyManagement = () => {
+  return (
+    <div className="px-4">
+      <h2>공통코드관리</h2>
+      <p>이곳은 공통코드관리 화면입니다.</p>
+    </div>
+  )
+}
+
+export default CompanyManagement
