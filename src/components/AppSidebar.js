680cd5b078dc740f420ddf0dd2b3f085e93437d9
Jaeho Lee <jaeho_1987@naver.com>
2025-05-05 12:44:54 +0900

    수정

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,28 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-
 import {
   CCloseButton,
   CSidebar,
   CSidebarBrand,
-  CSidebarFooter,
+  // CSidebarFooter,
   CSidebarHeader,
-  CSidebarToggler,
+  CSidebarNav,
+  // CSidebarToggler,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-
+import * as iconSet from '@coreui/icons'
+import { CNavItem, CNavGroup } from '@coreui/react'
 import { AppSidebarNav } from './AppSidebarNav'
-
 import { logo } from 'src/assets/brand/logo'
 import { sygnet } from 'src/assets/brand/sygnet'
-
-// sidebar nav config
-import navigation from '../_nav'
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const sidebarShow = useSelector((state) => state.sidebarShow)
+  const [menuItems, setMenuItems] = useState([])
+
+  useEffect(() => {
+    fetch('/api/menus', { credentials: 'include' })
+      .then((res) => res.json())
+      .then((data) => {
+        const tree = buildMenuTree(data)
+        const nav = convertToNavFormat(tree)
+        setMenuItems(nav)
+      })
+  }, [])
+
+  const buildMenuTree = (list) => {
+    const map = {}
+    const roots = []
+    list.forEach((item) => {
+      map[item.menuSeq] = { ...item, children: [] }
+    })
+    list.forEach((item) => {
+      if (item.parentSeq && map[item.parentSeq]) {
+        map[item.parentSeq].children.push(map[item.menuSeq])
+      } else {
+        roots.push(map[item.menuSeq])
+      }
+    })
+    return roots
+  }
+
+  const convertToNavFormat = (treeList) => {
+    return treeList.map((item) => {
+      const icon = item.menuIcon ? (
+        <CIcon icon={iconSet[item.menuIcon]} customClassName="nav-icon" />
+      ) : undefined
+
+      if (item.children.length > 0) {
+        return {
+          component: CNavGroup,
+          name: item.menuName,
+          icon,
+          to: item.menuPath || undefined,
+          items: convertToNavFormat(item.children),
+        }
+      } else {
+        return {
+          component: CNavItem,
+          name: item.menuName,
+          icon,
+          to: item.menuPath,
+        }
+      }
+    })
+  }
 
   return (
     <CSidebar
@@ -46,12 +95,16 @@ const AppSidebar = () => {
           onClick={() => dispatch({ type: 'set', sidebarShow: false })}
         />
       </CSidebarHeader>
-      <AppSidebarNav items={navigation} />
-      <CSidebarFooter className="border-top d-none d-lg-flex">
-        <CSidebarToggler
-          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-        />
-      </CSidebarFooter>
+
+      <CSidebarNav>
+        <AppSidebarNav items={menuItems} />
+      </CSidebarNav>
+
+      {/*<CSidebarFooter className="border-top d-none d-lg-flex">*/}
+      {/*  <CSidebarToggler*/}
+      {/*    onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}*/}
+      {/*  />*/}
+      {/*</CSidebarFooter>*/}
     </CSidebar>
   )
 }
